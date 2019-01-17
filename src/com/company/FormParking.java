0208fa0026c7d96657f402eb6da21382e4d5faa3
Tsarevich01 <pytinvovan@mail.ru>
2019-01-17 13:14:25 +0400

    add

--- src/com/company/FormParking.java
+++ src/com/company/FormParking.java
@@ -2,9 +2,11 @@ package com.company;
 
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,8 +16,10 @@ public class FormParking extends JFrame {
     private JFrame frame;
     private JPanel contentPane;
     private JTextField textField;
-    private Itruck transport;
-    private Parking<Itruck> parking;
+    private Itruck truck;
+    private MultiLevelParking parking; // / Îáúåêò îò êëàññà ìíîãîóðîâíåâîé
+    // ïàðêîâêè
+    private final int countLevel = 5; // Êîëè÷åñòâî óðîâíåé-ïàðêîâîê
 
     /**
      * Launch the application.
@@ -32,71 +36,107 @@ public class FormParking extends JFrame {
             }
         });
     }
-
     /**
      * Create the application.
      */
     public FormParking() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 1100, 503);
+        setBounds(100, 100, 1200, 550);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
-        PanelParking panelParking = new PanelParking();
-        panelParking.setBounds(10, 11, 639, 432);
-        contentPane.add(panelParking);
-        parking = new Parking<Itruck>(3, panelParking.getWidth(),
-                panelParking.getHeight());
 
+        PanelParking panelParking = new PanelParking();
+        panelParking.setBounds(10, 11, 630, 432);
+        contentPane.add(panelParking);
+
+        DefaultListModel listModel = new DefaultListModel();
+        for (int i = 0; i < countLevel; i++) {
+            listModel.addElement("Level " + Integer.toString(i + 1));
+        }
+
+        JList listLevels = new JList(listModel);
+        listLevels.setBounds(701, 40, 170, 124);
+        contentPane.add(listLevels);
+        listLevels
+                .setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        listLevels.setSelectedIndex(0);
+
+        ListSelectionListener listSelectionListener = new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                panelParking.repaint();
+            }
+        };
+        listLevels.addListSelectionListener(listSelectionListener);
+
+        parking = new MultiLevelParking(countLevel, panelParking.getWidth(),
+                panelParking.getHeight());
         panelParking.setParking(parking);
+        panelParking.setList(listLevels);
 
         JButton buttonSetTruck = new JButton(
                 "\u041F\u0440\u0438\u043F\u0430\u0440\u043A\u043E\u0432\u0430\u0442\u044C \u0433\u0440\u0443\u0437\u043E\u0432\u0438\u043A");
         buttonSetTruck.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-
-                Color firstColor = JColorChooser.showDialog(null,
-                        "Choose a Color", Color.WHITE);
-
-                transport = new Truck(100, 1000, firstColor);
-                parking.addTransport(transport);
-                panelParking.repaint();
+                if (listLevels.getSelectedIndex() > -1) {
+                    Color firstColor = JColorChooser.showDialog(null,
+                            "Choose a Color", Color.WHITE);
+                    truck = new Truck(100, 1000, firstColor);
+                    int place = parking.getParking(
+                            listLevels.getSelectedIndex()).addTransport(
+                            truck);
+                    if (place != -1) {
+                        panelParking.repaint();
+                    }
+                    panelParking.repaint();
+                }
             }
         });
-        buttonSetTruck.setBounds(665, 19, 245, 70);
+        buttonSetTruck.setBounds(665, 167, 245, 25);
         contentPane.add(buttonSetTruck);
         JButton buttonSetTruckTrailer = new JButton(
                 "\u041F\u0440\u0438\u043F\u0430\u0440\u043A\u043E\u0432\u0430\u0442\u044C \u0433\u0440\u0443\u0437\u043E\u0432\u0438\u043A-\u043F\u043E\u043B\u0443\u043F\u0440\u0438\u0446\u0435\u043F");
         buttonSetTruckTrailer.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-
-                Color firstColor = JColorChooser.showDialog(null,
-                        "Choose a Color", Color.WHITE);
-                Color secondColor = JColorChooser.showDialog(null,
-                        "Choose a Color", Color.WHITE);
-                transport = new Benzovoz(100, 1000, firstColor,
-                        secondColor);
-                parking.addTransport(transport);
-                panelParking.repaint();
+                if (listLevels.getSelectedIndex() > -1) {
+                    Color firstColor = JColorChooser.showDialog(null,
+                            "Choose a Color", Color.WHITE);
+                    Color secondColor = JColorChooser.showDialog(null,
+                            "Choose a Color", Color.WHITE);
+                    truck = new Benzovoz(100, 1000, firstColor,
+                            secondColor);
+                    int place = parking.getParking(
+                            listLevels.getSelectedIndex()).addTransport(
+                            truck);
+                    if (place != -1) {
+                        panelParking.repaint();
+                    }
+                    panelParking.repaint();
+                }
             }
         });
-        buttonSetTruckTrailer.setBounds(665, 100, 245, 70);
+        buttonSetTruckTrailer.setBounds(665, 205, 245, 25);
         contentPane.add(buttonSetTruckTrailer);
         JPanel panelGroupElements = new JPanel();
-        panelGroupElements.setBounds(665, 183, 400, 500);
+        panelGroupElements.setBounds(665, 244, 500, 260);
         contentPane.add(panelGroupElements);
         panelGroupElements.setLayout(null);
         JLabel lblNewLabel = new JLabel("\u041C\u0435\u0441\u0442\u043E");
         lblNewLabel.setBounds(26, 14, 40, 14);
         panelGroupElements.add(lblNewLabel);
         PanelForPaint panelTakeTrain = new PanelForPaint();
-        panelTakeTrain.setBounds(10, 72, 360, 200);
+        panelTakeTrain.setBounds(10, 72, 400, 200);
         panelGroupElements.add(panelTakeTrain);
         JButton buttonTakeTruck = new JButton(
                 "\u0417\u0430\u0431\u0440\u0430\u0442\u044C ");
         buttonTakeTruck.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
+
+                if (listLevels.getSelectedIndex() == -1) {
+                    return;
+                }
                 int numberOfPlace = 0;
                 try {
                     numberOfPlace = Integer.parseInt(textField.getText());
@@ -104,27 +144,28 @@ public class FormParking extends JFrame {
                     textField.setText("Invalid input");
                     return;
                 }
-                if (numberOfPlace >= parking._places.size()
-                        || numberOfPlace < 0) {
-                    textField.setText("Invalid input");
-                    return;
-                }
-                transport = parking.removeTransport(numberOfPlace);
-                if (transport != null) {
-                    transport.SetPosition(5, 5, panelTakeTrain.getWidth(),
+                truck = parking.getParking(listLevels.getSelectedIndex())
+                        .removeTransport(numberOfPlace);
+                if (truck != null) {
+                    truck.SetPosition(5, 5, panelTakeTrain.getWidth(),
                             panelTakeTrain.getHeight());
                 }
-                panelTakeTrain.setTruck(transport);
+                panelTakeTrain.setTruck(truck);
                 panelTakeTrain.repaint();
                 panelParking.repaint();
             }
         });
+
         buttonTakeTruck.setBounds(10, 39, 223, 23);
         panelGroupElements.add(buttonTakeTruck);
         textField = new JTextField();
         textField.setBounds(78, 11, 79, 20);
         panelGroupElements.add(textField);
         textField.setColumns(10);
+
+        JLabel levelsLabel = new JLabel("\u0423\u0440\u043E\u0432\u043D\u0438:");
+        levelsLabel.setBounds(756, 11, 56, 16);
+        contentPane.add(levelsLabel);
     }
 
     /**
@@ -135,5 +176,4 @@ public class FormParking extends JFrame {
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
 }
--- src/com/company/PanelParking.java
+++ src/com/company/PanelParking.java
@@ -2,14 +2,20 @@ package com.company;
 
 import java.awt.Graphics;
 
+import javax.swing.JList;
 import javax.swing.JPanel;
 
 
 public class PanelParking extends JPanel {
-    private Parking<Itruck> parking;
+    private MultiLevelParking parking;
+    private JList listLevels;
 
-    public void setParking(Parking parking) {
+    public void setParking(MultiLevelParking parking) {
         this.parking = parking;
+    }
+
+    public void setList(JList listLevels) {
+        this.listLevels = listLevels;
     }
 
     @Override
@@ -17,7 +23,9 @@ public class PanelParking extends JPanel {
         super.paint(g);
         try {
             if (parking != null) {
-                parking.Draw(g);
+                if (listLevels.getSelectedIndex() != -1) {
+                    parking.getParking(listLevels.getSelectedIndex()).Draw(g);
+                }
             }
         } catch (Exception ex) {
         }
